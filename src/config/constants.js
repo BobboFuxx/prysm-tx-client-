1343f107912ba465fc3acef8ca66d335aef076b0
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2024-12-05 13:16:12 -0800

    Create constants.js

---
+++ src/config/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+  RPC_ENDPOINT: "https://prysm-testnet-rpc.itrocket.net",
+  API_ENDPOINT: "https://prysm-testnet-api.itrocket.net",
+  CHAIN_ID: "prysm_100-1",
+  DENOM: "uprysm",
+};
